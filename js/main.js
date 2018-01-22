28fb2e7f172509c823782f2d5e2a03ae4297d093
AlbertoSP <ALBERTOSANCHEZHTML@GMAIL.COM>
2018-01-22 21:22:24 +0100

    he añadido un muro

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-
-
-$( document ).ready(function() {
-    var miPaleta   = new Paleta();
+$(document).ready(function () {
+    var miPaleta = new Paleta();
     miPaleta.drawPaleta();
-    
-    $(document).keydown(function(e){
+
+    $(document).keydown(function (e) {
         miPaleta.move(e)
     });
-
+    $(document).ready(function () {
+        var miMuro = new Muros();
+        miMuro.drawMuros();
+    });
 });
-   
--- js/muros.js
+++ js/muros.js
@@ -1,3 +1,10 @@
- function Muros() {
-
- };
+function Muros() {
+    this.positionTop= 200;
+   this.positionLeft= 35;
+   }
+   Muros.prototype.drawMuros = function () {
+    muro = document.createElement('div')
+    $(".gameContainer").append(muro);
+    $(muro).addClass("muro");
+    $(muro).css({"top": this.positionTop,"left": this.positionLeft});
+}
