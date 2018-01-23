f148b549cf70c384a57020173a06113918846855
AlbertoSP <ALBERTOSANCHEZHTML@GMAIL.COM>
2018-01-23 17:06:36 +0100

    el muro se mueve

--- js/main.js
+++ js/main.js
@@ -6,19 +6,12 @@ $(document).ready(function () {
         miPaleta.move(e)
     });
     $(document).ready(function () {
-        var miMuro1 = new Muro(20, 20);
-        // var miMuro2=new Muro();
-        // var miMuro3=new Muro();
-        miMuro1.drawMuros()
-
-
-        // setInterval(function () {
-        //     miMuro1.move();
-        // //ejecutar ua funcion un número de veces
+        var miMuro1 = new Muro(20, 100, 10);
        
-        // }, 2000);
-  
-        // miMuro3.drawMuros(170, 40);
+        setInterval(function () {
+            miMuro1.move();
+            //ejecutar ua funcion un número de veces
+        }, 10);
     });
     $(document).ready(function () {
         var miCanasta = new Canasta();
--- js/muro.js
+++ js/muro.js
@@ -1,23 +1,49 @@
-function Muro(top, left) {
-    this.top = top
-    this.left = left
-}
+// function Muro(top, left, speed) {
+//     this.top = top
+//     this.left = left
+//     this.speed = speed;
+// }
+ 
+// Muro.prototype.drawMuros = function () {
+//     muro = document.createElement('div')
+//     $(".gameContainer").append(muro);
+//     $(muro).addClass("muro");
+//     $(muro).css({
+//         "top": this.top,
+//         "left": this.left
+//     });
 
-Muro.prototype.drawMuros = function () {
-    muro = document.createElement('div')
-    $(".gameContainer").append(muro);
-    $(muro).addClass("muro");
-    $(muro).css({
-        "top": this.top,
-        "left": this.left
-    });
+// }
+// Muro.prototype.move = function () {
+//     this.left += this.speed;
+//     this.muro.css({
+//         top: this.top,
+//         left: this.left,
+//         position: "absolute"
+//     });
 
-}
-Muro.prototype.move = function () {
-    
-    3
-    
-}
+//     if (this.left >= $(“.gameContainer”).width() || this.left <= 0) {
+//         this.speed *= -1;
+//     }
+// };
+
 
 
 //css("propertyname","value");
+function Muro(x, y, speed){
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.element = $("<div>").attr("class","muro");
+    this.element.css({top:this.y, left:this.x, position:"ansolute"});
+    $(".gameContainer").append(this.element);
+   }
+   
+   Muro.prototype.move = function(){
+    this.x += this.speed;
+    this.element.css({top:this.y, left:this.x, position:"relative"});
+   
+    if(this.x >= $(".gameContainer").width() || this.x <= 0){
+      this.speed *= -1;
+    }
+   };
